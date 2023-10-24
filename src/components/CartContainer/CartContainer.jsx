56772b4314a6d11089d1efefb2ee766ad1b741cc
Valen Quimey <valenkondra11@gmail.com>
2023-10-24 09:12:18 -0300

    nuevo commit

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,22 +1,33 @@
-import { useCartContext } from "../../context/CartContext"
+import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/CartContext";
 
 const CartContainer = () => {
+  const { cartList, deleteCart, eliminarProducto, precioTotal } = useCartContext();
 
-
-  const {cartList, deleteCart, eliminarProducto, precioTotal} = useCartContext()
-
-  
   return (
     <div>
-      {cartList.map(prod => <div key={prod.id}>
-          <img src={prod.imageUrl} alt={prod.name} className="w-25"/>
-          {prod.name} - ${prod.price} - Cantidad: {prod.quantity}
-          <button className="btn btn-danger" onClick={ () => eliminarProducto(prod.id) }> X </button>
-      </div>)}
-      <button onClick={deleteCart}>Vaciar Carrito</button>
-        <h3>Precio total: {precioTotal()}</h3>
+      {cartList.length === 0 ? (
+        <div>
+          <p>Tu carrito está vacío.</p>
+          <Link to="/">Volver a Inicio</Link>
+        </div>
+      ) : (
+        <div>
+          {cartList.map((prod) => (
+            <div key={prod.id}>
+              <img src={prod.imageUrl} alt={prod.name} className="w-25" />
+              {prod.name} - ${prod.price} - Cantidad: {prod.quantity}
+              <button className="btn btn-danger" onClick={() => eliminarProducto(prod.id)}>
+                X
+              </button>
+            </div>
+          ))}
+          <button onClick={deleteCart}>Vaciar Carrito</button>
+          <h3>Precio total: {precioTotal()}</h3>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CartContainer
+export default CartContainer;
--- src/components/ItemDetailContainer/ItemDetialContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetialContainer.jsx
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-
 import { mFetch } from "../../utils/mockFetch"
 import ItemDetail from "./ItemDetail/ItemDetail"
-
-
-
 
 const ItemDetialContainer = () => {
     // api manejo de estados etc
     const [product, setProduct ] = useState({})
+   const [ loading, setLoading ] = useState(true)
     const { pid } = useParams()
     console.log(pid)
 
-    useEffect(()=>{
-        // llamada a la api
-        mFetch(Number(pid))
-        .then(resp => setProduct(resp))
-        .catch(err=> console.log(err))
-        // .finally(set loading)
-    },[pid])
-
+   useEffect(()=>{
+// llamada a la api
+mFetch(Number(pid))
+.then(resp => setProduct(resp))
+.catch(err=> console.log(err))
+.finally(()=> setLoading(false))
+},[pid])
     return (
         <div>            
-            <ItemDetail product={product} />           
+           {loading ? <h1>Loading...</h1> : <ItemDetail product={product} /> }          
         </div>
     )
 }
